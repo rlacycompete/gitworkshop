d09ac449fb7852d63c815d9e5ffa7024f41cef7e
Rob Lacy <rlacy@compete.com>
2013-02-01 07:00:41 -0500

    added verify

---
+++ verify.py
@@ -0,0 +1,21 @@
+# roses
+roses = None
+
+# violets
+violets = None
+
+# this
+this_exercise = None
+
+# and
+and_so_are = None
+
+if(
+    roses == 'red' and
+    violets == 'blue' and
+    this_exercise == 'fun' and
+    and_so_are == 'you'
+):
+    print 'Success!'
+else:
+    print 'Nope'
